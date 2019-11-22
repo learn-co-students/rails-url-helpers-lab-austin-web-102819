4695b746df53603e79fa096ea20cb14a0c09b5c0
rachfuerst <rach.fuerst@gmail.com>
2019-11-22 15:51:36 -0600

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,8 +6,19 @@ class StudentsController < ApplicationController
   end
 
   def show
+    @student= Student.find(params[:id])
   end
 
+  def activate
+    # Create the instance variable @student and set it equal to the student with the same id as what is input in the params
+    @student= Student.find(params[:id])
+    # Set the student's active attribute equal to whatever is it's opposite. If it's true, set it to false, vice versa
+    @student.active = !@student.active 
+    # Save these changes to the database
+    @student.save
+    # Redirect the student to their path. 
+    redirect_to student_path(@student)
+  end
   private
 
     def set_student
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :students, only: [:index, :show]
+  # Here we're configuring a route to allow a user to toggle their "active" attribute
+  # Route a GET request from /students/:id/activate to the app/controllers/students_controller.rb's #activate method
+  # Name this route: activate_student
+  get '/students/:id/activate', to: 'students#activate', as: 'activate_student'
 end
