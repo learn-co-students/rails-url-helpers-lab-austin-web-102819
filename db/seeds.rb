f628f7539cc5b02648873152b40c0602730a0535
BrittneyHaselmayer <brittneyhaselmayer@yahoo.com>
2019-11-24 18:28:02 -0600

    Done.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+Student.create(first_name: "Brittney", last_name: "Haselmayer" , active: true)
+Student.create(first_name: "Joe" , last_name: "Dee" , active: false)
